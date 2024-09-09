eba0ae38dd8f0ae57c97c2ece909c234d0abcc6a
Alonum <a249664@dac.unicamp.br>
2024-09-09 20:21:30 -0300

    added title and fixed blur. +created javascript file for functions that will be used on every topic

---
+++ generalScript.js
@@ -0,0 +1,82 @@
+function openPDF(string){
+    window.open(string,'_blank');
+}
+
+  function expandButton(imageLocStringEx,imageLocStringClo, hiddenId, expandId) {
+      var hiddenButtonsId = document.getElementById(hiddenId);
+      var expandButtonsId = document.getElementById(expandId)
+      //console.log(expandButtonsId);
+      console.log(expandButtonsId.parentElement.id)
+      var imgElement = expandButtonsId.querySelector('img');
+      
+    
+      if (hiddenButtonsId.style.display === "none" || hiddenButtonsId.style.display === "") {
+        hiddenButtonsId.style.display = "block";
+          imgElement.src = imageLocStringClo;
+          imgElement.alt = "Fechar";
+          blurOthers(expandButtonsId.parentElement.id);
+      } else {
+          hiddenButtonsId.style.display = "none";
+          imgElement.src = imageLocStringEx;
+          imgElement.alt = "Expandir";
+          unblurOthers(expandButtonsId.parentElement.id);
+      }
+  };
+  function blur(id)
+  {
+    let divId = document.getElementById(id)
+    divId.classList.add('blur')
+  }
+  function unblur(id)
+  {
+    let divId = document.getElementById(id)
+    divId.classList.remove('blur')
+  }
+  function blurOthers(id){
+    let divId = document.getElementById(id)
+    //console.log(id);
+    blur("returnButton");
+    blur("pageTitle");
+
+    if(divId == document.getElementById("PDFDiv")){
+      blur("RefDiv")
+      blur("ExDiv")
+    }
+    if(divId == document.getElementById("RefDiv")){
+      blur("PDFDiv")
+      blur("ExDiv")
+    }
+    if(divId == document.getElementById("ExDiv")){
+      blur("PDFDiv")
+      blur("RefDiv")
+    }
+  };
+
+  function unblurOthers(id){
+    let divId = document.getElementById(id)
+    unblur("returnButton");
+    unblur("pageTitle");
+    if(divId == document.getElementById("PDFDiv")){
+      unblur("RefDiv")
+      unblur("ExDiv")
+    }
+    if(divId == document.getElementById("RefDiv")){
+      unblur("PDFDiv")
+      unblur("ExDiv")
+    }
+    if(divId == document.getElementById("ExDiv")){
+      unblur("PDFDiv")
+      unblur("RefDiv")
+    }
+  };
+
+  function moveAbove(id){
+    let elementId = document.getElementById(id);
+
+    if(elementId.classList.contains("moveAbove")){
+      elementId.classList.remove("moveAbove");
+      
+    }else{
+      elementId.classList.add("moveAbove");   
+    }
+  }
